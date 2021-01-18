c70c8d39469ab20894737362e71d49a5477f3a1c
vladsolntsev <vlad.solnt@gmail.com>
2021-01-18 10:55:42 +0100

    3 quest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
 import './App.css';
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 
 function App() {
   return (
       <div className="App">
-          <Contact
-              name="Jeff Soto"
-              avatar="https://randomuser.me/api/portraits/men/29.jpg"
-              online="online"
-          />
-          <Contact
-              name="Fely Black"
-              avatar="https://randomuser.me/api/portraits/women/87.jpg"
-              online=""
-          />
-          <Contact
-              name="Aly Watts"
-              avatar="https://randomuser.me/api/portraits/women/2.jpg"
-              online="online"
-          />
+          <ContactList />
       </div>
   );
 }
